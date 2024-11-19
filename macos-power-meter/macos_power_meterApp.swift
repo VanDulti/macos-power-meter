1d0675fc327b955b0c084f47cb32297a9f0a1585
VanDulti <me@ulrich-dultinger.dev>
2024-11-19 15:05:35 +0100

    Initial Commit

---
+++ macos-power-meter/macos_power_meterApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_power_meterApp.swift
+//  macos-power-meter
+//
+//  Created by Ulrich Dultinger on 19.11.24.
+//
+
+import SwiftUI
+
+@main
+struct macos_power_meterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
